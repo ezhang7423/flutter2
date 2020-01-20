72199b29c7da2cff43d7af7cd02df35f6c0112be
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2020-01-19 20:45:00 -0800

    ios is hard

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'screens/location_detail/location_detail.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       home: LocationDetail(),
     );
   }
--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,13 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello'),
-      ),
-      body: Center(child: Text('was gud ma bois')),
-    );
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), title: Text('Hello')),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+        ]),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ProductListTab(),
+                );
+              });
+            case 1:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: SearchTab(),
+                );
+              });
+          }
+        });
   }
 }
