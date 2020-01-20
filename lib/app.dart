5880dadde968c3d2f57c24ab2fda4da58e5cd351
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2020-01-19 20:47:45 -0800

    back to material

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'screens/location_detail/location_detail.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: LocationDetail(),
     );
   }
