614f903c96420d711dbde4525a81e047c82635ee
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2020-01-21 11:07:28 -0800

    testing container

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -4,22 +4,25 @@ class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Hello')),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                  ),
-                  child: Text('was gud ma boi')),
-              Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text('was gud ma boi')),
-              Container(child: Text('was gud ma boi')),
-            ]));
+      appBar: AppBar(title: Text('Hello')),
+      body: Container(
+          decoration: BoxDecoration(color: Colors.green[400]),
+          child: Text('hello'),
+          alignment: Alignment.center,
+    );
+    // body: Column(
+    //     mainAxisAlignment: MainAxisAlignment.start,
+    //     crossAxisAlignment: CrossAxisAlignment.stretch,
+    //     children: [
+    //       Container(
+    //           decoration: BoxDecoration(color: Colors.amber),
+    //           child: Text('was gud ma boi')),
+    //       Container(
+    //           decoration: BoxDecoration(
+    //             color: Colors.blue,
+    //           ),
+    //           child: Text('was gud ma boi')),
+    //       Container(child: Text('was gud ma boi')),
+    //     ]));
   }
 }
